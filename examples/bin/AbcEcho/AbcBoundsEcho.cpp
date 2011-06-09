2688bf971078ab20406f7f4717214464faa13137
Joe Ardent <alembic@ardent.nebcorp.com>
2011-06-08 19:40:49 -0700

    don't assume the object is xform

--- examples/bin/AbcEcho/AbcBoundsEcho.cpp
+++ examples/bin/AbcEcho/AbcBoundsEcho.cpp
@@ -49,10 +49,14 @@ static Box3d g_bounds;
 //-*****************************************************************************
 void accumXform( M44d &xf, IObject obj )
 {
-    IXform x( obj, kWrapExisting );
-    XformSample xs;
-    x.getSchema().get( xs );
-    xf *= xs.getMatrix();
+    //IObject parent = obj.getParent();
+    if ( IXform::matches( obj.getHeader() ) )
+    {
+        IXform x( obj, kWrapExisting );
+        XformSample xs;
+        x.getSchema().get( xs );
+        xf *= xs.getMatrix();
+    }
 }
 
 //-*****************************************************************************
