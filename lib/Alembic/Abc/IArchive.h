5a76a1fb86068c293add2e57ed009d0796d4b433
Joe Ardent <alembic@ardent.nebcorp.com>
2011-08-04 17:23:49 -0700

    namespace Abc with help from Lucas' script

--- lib/Alembic/Abc/IArchive.h
+++ lib/Alembic/Abc/IArchive.h
@@ -43,6 +43,7 @@
 
 namespace Alembic {
 namespace Abc {
+namespace ALEMBIC_VERSION_NS {
 
 class IObject;
 
@@ -191,6 +192,10 @@ IArchive::IArchive( ARCHIVE_CTOR iCtor,
     ALEMBIC_ABC_SAFE_CALL_END_RESET();
 
 }
+
+} // End namespace ALEMBIC_VERSION_NS
+
+using namespace ALEMBIC_VERSION_NS;
 
 } // End namespace Abc
 } // End namespace Alembic
